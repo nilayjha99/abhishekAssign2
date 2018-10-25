223ac973c0eaa226666376a82789e0b1a446e3e8
Administrator <aiv700@udml14.cs.uregina.ca>
2018-10-24 23:19:50 -0600

    buton editing

---
+++ assignment2/PrioritySelector.swift
@@ -0,0 +1,62 @@
+//
+//  PrioritySelector.swift
+//  assignment2
+//
+//  Created by Abhishekkumar Israni on 2018-10-24.
+//  Copyright © 2018 Abhishekkumar Israni. All rights reserved.
+//
+
+import UIKit
+
+class PrioritySelector: UIStackView {
+
+    //MARK Instialisation
+    
+    override init(frame: CGRect) {
+         super.init(frame : frame)
+        setupPriorityButtons()
+    }
+    
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupPriorityButtons()
+    }
+    
+    
+    //MARK Private Methods
+    
+    private func setupPriorityButtons() {
+        
+        
+        //define red(high) priority button
+        let priorityButton1=UIButton()
+        priorityButton1.backgroundColor = UIColor.red
+        //priorityButton1.translatesAutoresizingMaskIntoConstraints = false
+        //priorityButton1.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        //priorityButton1.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        // Add the button to the stack
+        addArrangedSubview(priorityButton1)
+        
+       /* ////define orange(medium) priority button
+        let priorityButton2=UIButton()
+        priorityButton2.backgroundColor = UIColor.orange
+        priorityButton2.backgroundColor = UIColor.red
+        priorityButton2.translatesAutoresizingMaskIntoConstraints = false
+        priorityButton2.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        priorityButton2.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        addArrangedSubview(priorityButton2)
+        
+        //define green(low) priority button
+        let priorityButton3=UIButton()
+        priorityButton3.backgroundColor = UIColor.green
+        priorityButton3.backgroundColor = UIColor.red
+        priorityButton3.translatesAutoresizingMaskIntoConstraints = false
+        priorityButton3.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        priorityButton3.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        addArrangedSubview(priorityButton3)
+    */
+    }
+    
+    
+}
--- assignment2/ViewController.swift
+++ assignment2/ViewController.swift
@@ -9,12 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var datePickerTF: UITextField!
+    
+    //Properties
+    let datePicker=UIDatePicker()
+    
+
+    //Actions
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //function call here
+        createDatePicker()
     }
-
-
+    
+    //will popup date picker when text field is tapped
+    func createDatePicker(){
+        
+        //Formatting the display
+        datePicker.datePickerMode = .date
+        
+        //assigning date picker to textfied
+        datePickerTF.inputView=datePicker
+    
+    
+    // creating a tool bar
+        let toolBar=UIToolbar()
+        toolBar.sizeToFit()
+        
+    //adding a "done" button on the tool bar
+        
+        let doneButton=UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneClicked))
+        
+        let immediateButton = UIBarButtonItem(title: "Immediate", style: .done, target: nil, action: #selector(setTextFieldImmediate))
+        
+        let UnSpecified = UIBarButtonItem(title: "Unspecified", style: .done, target: nil, action: #selector(unSpecified))
+        
+        toolBar.setItems([doneButton,immediateButton,UnSpecified], animated: true)
+        
+        datePickerTF.inputAccessoryView = toolBar
+    }
+    
+    // for assigning value to the text field when "done" pressed
+    @objc func doneClicked(){
+        
+        //format of the display
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+         dateFormatter.timeStyle = .medium
+        datePickerTF.text = dateFormatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+        
+    }
+    
+    // sets value of the text field as Immediate
+    @objc func setTextFieldImmediate()
+    {
+        datePickerTF.text = "Immediate"
+        self.view.endEditing(true)
+        
+    }
+    
+    
+    // Selects value of the text Field as Unspecified
+    @objc func unSpecified()
+    {
+        datePickerTF.text = "Unspecified"
+        self.view.endEditing(true)
+    }
+    
+    
 }
 
